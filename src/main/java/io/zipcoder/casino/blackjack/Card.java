2d9d1ebdbc3ca727754aafbd06c67101c7f5a76e
Arcelia Calleja <arceliacalleja@gmail.com>
2019-02-26 18:19:16 -0500

    getters and setters done

--- src/main/java/io/zipcoder/casino/blackjack/Card.java
+++ src/main/java/io/zipcoder/casino/blackjack/Card.java
@@ -1,6 +1,26 @@
 package io.zipcoder.casino.blackjack;
 
 public class Card {
+
+    private Suite suite;
+    private FaceValue faceValue;
+
+    public Suite getSuite() {
+        return suite;
+    }
+
+    public FaceValue getFaceValue() {
+        return faceValue;
+    }
+
+    public void setSuite(Suite suite) {
+        this.suite = suite;
+    }
+
+    public void setFaceValue(FaceValue faceValue) {
+        this.faceValue = faceValue;
+    }
+
     //declare a field of type Suite named suite
     //declare a field of type FaceValue named faceValue
 
